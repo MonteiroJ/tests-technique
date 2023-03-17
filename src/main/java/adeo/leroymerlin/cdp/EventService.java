75e8e283b676c7154ba21f3316113e959302fad7
MonteiroJ <josemonteiro.baggio@gmail.com>
2023-03-17 19:52:47 +0100

    feature: Update the search Api to use the query parameters

--- src/main/java/adeo/leroymerlin/cdp/EventService.java
+++ src/main/java/adeo/leroymerlin/cdp/EventService.java
@@ -3,7 +3,9 @@ package adeo.leroymerlin.cdp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -27,6 +29,27 @@ public class EventService {
         List<Event> events = eventRepository.findAllBy();
         // Filter the events list in pure JAVA here
 
-        return events;
+        return events.stream()
+                .filter(event -> event.getBands().stream()
+                        .anyMatch(band -> band.getMembers().stream()
+                                .anyMatch(member -> member.getName().contains(query))))
+                // Filter events where any of their bands have members matching the query
+                .peek(event -> {
+
+                    List<Band> filteredBands = event.getBands().stream()
+                            .filter(band -> band.getMembers().stream()
+                                    .anyMatch(member -> member.getName().contains(query)))
+                            // Filter bands where any of their members match the query
+                            .peek(band -> {
+                                List<Member> members = band.getMembers().stream()
+                                        .filter(member -> member.getName().contains(query))
+                                        .collect(Collectors.toList()); // Filter members matching the query and collect them in a list
+                                band.setMembers(new HashSet<>(members));
+                            })
+                            .collect(Collectors.toList()); // Collect the filtered bands in a list
+
+                    event.setBands(new HashSet<>(filteredBands)); // Set the filtered bands on the event, converting the list to a set
+                })
+                .collect(Collectors.toList()); // Collect the filtered events in a list
     }
 }
